5573bbec254a931fdffd6e8dda3f592c4cb3acfa
z.terehov <zaharkuli67@gmail.com>
2022-10-31 12:06:49 +0300

    Created and Implement Register page

---
+++ AspNetCoreIdentity/AspNetCoreIdentity/ViewModel/Register.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity.ViewModel
+{
+	public class Register
+	{
+		[Required]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		[Compare(nameof(Password), ErrorMessage = "Password and confirmation password did not math")]
+		public string ConfirmPassword { get; set; }
+	}
+}
