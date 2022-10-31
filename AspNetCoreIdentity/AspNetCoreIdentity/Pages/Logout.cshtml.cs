728ac7bac9d2996481d8ab573a0bece4c57bd32c
z.terehov <zaharkuli67@gmail.com>
2022-10-31 15:15:31 +0300

    Add authorization to page

---
+++ AspNetCoreIdentity/AspNetCoreIdentity/Pages/Logout.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreIdentity.Pages
+{
+    public class LogoutModel : PageModel
+    {
+		private readonly SignInManager<IdentityUser> _signInManager;
+
+		public LogoutModel(SignInManager<IdentityUser> _signInManager)
+		{
+			this._signInManager = _signInManager;
+		}
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+		{
+			await _signInManager.SignOutAsync();
+
+			return RedirectToPage("Login");
+		}
+
+		public IActionResult OnPostDontLogoutAsync()
+		{
+			return RedirectToPage("Index");
+		}
+    }
+}
