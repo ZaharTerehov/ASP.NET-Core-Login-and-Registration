5c4a70a2f6b080880d7c1ad5b76d25c64630be8f
z.terehov <zaharkuli67@gmail.com>
2022-10-31 09:25:57 +0300

    added EF Core

--- AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -1,6 +1,8 @@
+using AspNetCoreIdentity.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +26,7 @@ namespace AspNetCoreIdentity
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
+			services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("AuthConnectionString")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
